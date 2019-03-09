dc9ab97f438292903be5cf84f80fbaa16aa3d55f
Taki <exa.a.rocth.artway@gmail.com>
2019-03-09 14:46:11 +0900

    version1.5

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
+import os
 from setuptools import setup
+
+
+def get_packages(package):
+    return [dirpath for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
 
 
 setup(
@@ -10,9 +28,8 @@ setup(
     author_email='taki.kwi@gmail.com',
     license='MIT',
     keywords='rangefilter',
-    packages=[
-        "rangefilter",
-    ],
+    packages=get_packages('rangefilter'),
+    package_data=get_package_data('rangefilter'),
     include_package_data=True,
     classifiers=[
         'Programming Language :: Python :: 3.6',
